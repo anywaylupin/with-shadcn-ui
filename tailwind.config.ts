328da5724646552d7d5ee6a1ffe71839ea002e91
Lupin <lupin.nguyen@outlook.com>
2024-09-02 17:12:02 +0700

    feat: input and label

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,7 @@ const config = {
     },
     extend: {
       boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
         'beam-collision':
           '0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset'
       },
