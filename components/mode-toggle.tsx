ed965f3ab7d4b41bc383ccb4583c9b2ccf83b689
Lupin <lupin.nguyen@outlook.com>
2025-02-27 05:31:08 +0700

    refactor: update configuration files and improve component structure

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,18 +1,56 @@
 'use client';
 
-import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-
-import { Button } from '@/components/ui/button';
 
 export const ModeToggle = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Button variant="link" type="button" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-      <Sun className="text-neutral-800 dark:hidden dark:text-neutral-200" />
-      <Moon className="hidden text-neutral-800 dark:block dark:text-neutral-200" />
-    </Button>
+    <button className="cursor-pointer" type="button" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      <IconSun />
+      <IconMoon />
+    </button>
   );
 };
+
+const IconSun = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="text-neutral-800 dark:hidden dark:text-neutral-200"
+  >
+    <circle cx="12" cy="12" r="4" />
+    <path d="M12 2v2" />
+    <path d="M12 20v2" />
+    <path d="m4.93 4.93 1.41 1.41" />
+    <path d="m17.66 17.66 1.41 1.41" />
+    <path d="M2 12h2" />
+    <path d="M20 12h2" />
+    <path d="m6.34 17.66-1.41 1.41" />
+    <path d="m19.07 4.93-1.41 1.41" />
+  </svg>
+);
+
+const IconMoon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="hidden text-neutral-800 dark:block dark:text-neutral-200"
+  >
+    <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+  </svg>
+);
